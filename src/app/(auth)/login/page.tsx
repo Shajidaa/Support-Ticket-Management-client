90f99f3de479f3ad5160bc4d40a4c69ef5dffb50
Shajidaa <shajidaakterlopa101@gmail.com>
2026-08-24 21:46:36 +0600

    feat(auth): login and register page add border-beam

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,11 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { BorderBeam } from "@/components/ui/border-beam";
 import { APP_NAME } from "@/lib/constants";
 
 export default function LoginPage() {
   return (
-    <div className="grid gap-6">
+    <div className="relative h-80 w-full overflow-hidden">
+       <div className="grid gap-6 p-2">
       <div className="grid gap-1">
         <h1 className="font-heading text-2xl font-semibold">Sign in</h1>
         <p className="text-sm text-muted-foreground">
@@ -12,5 +14,9 @@ export default function LoginPage() {
       </div>
       <LoginForm />
     </div>
+ <BorderBeam />
+    </div>
+   
+
   );
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,8 +1,10 @@
 import { RegisterForm } from "@/components/auth/register-form";
+import { BorderBeam } from "@/components/ui/border-beam";
 
 export default function RegisterPage() {
   return (
-    <div className="grid gap-6">
+    <div className="relative h-125 w-full overflow-hidden">
+ <div className="grid gap-6 p-2 ">
       <div className="grid gap-1">
         <h1 className="font-heading text-2xl font-semibold">Create an account</h1>
         <p className="text-sm text-muted-foreground">
@@ -10,6 +12,10 @@ export default function RegisterPage() {
         </p>
       </div>
       <RegisterForm />
+      
     </div>
+      <BorderBeam />
+    </div>
+   
   );
 }
